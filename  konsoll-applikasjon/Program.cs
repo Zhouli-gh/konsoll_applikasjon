cd05a675c928f4446543caffecd38c06dc3d4e15
zhouli <zhoulikvaal@gmail.com>
2025-05-07 19:16:08 +0200

    konsoll_applikasjon

---
+++  konsoll-applikasjon/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace konsoll_applikasjon;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 5;
+        decimal b = 3;
+        decimal sum = a + b;
+        Console.WriteLine($"sum = {sum}");
+    }
+}
+/*
+ * Oppgave 2: 
+   
+   Lag en konsoll-applikasjon som lager 3 variabler. De kan hete a, b og sum. 
+   - a skal være en int. 
+   - b skal være en decimal. 
+   
+   Verdiene variablene skal ha er: 
+   
+   - a = 5;
+   - b = 3;
+   - sum = a + b;
+ */
